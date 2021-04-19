4a5082f7bab63426e735a347834528cab94f1fc0
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-04-19 16:45:33 +0200

    Further NPM installs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+const config: SocketIoConfig = {url: 'http://localhost:3100', options: {}}
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
