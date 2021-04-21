a1a57396ea749b3a2e44fb4813217e820bba0cc6
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-04-21 14:19:25 +0200

    Header & nav-bar update

--- src/app/page-basics/header/header.component.ts
+++ src/app/page-basics/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  searchRedirect(event: KeyboardEvent): void{
+    this.router.routeReuseStrategy.shouldReuseRoute = function() {
+      return false;
+    };
+
+    let searchString: string = (event.target as HTMLInputElement).value.trim();
+    this.router.navigate(['/shop/', searchString]);
+  }
+
 
 }
