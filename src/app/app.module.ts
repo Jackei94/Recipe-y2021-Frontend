68468bbf793d028c55c73e9354e981cd1be430bb
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-04-21 14:32:15 +0200

    Added page-layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NavBarComponent } from './page-basics/nav-bar/nav-bar.component';
-import { HeaderComponent } from './page-basics/header/header.component';
-import { FooterComponent } from './page-basics/footer/footer.component';
+import { NavBarComponent } from './page-layout/nav-bar/nav-bar.component';
+import { HeaderComponent } from './page-layout/header/header.component';
+import { FooterComponent } from './page-layout/footer/footer.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import {PageLayoutModule} from "./page-layout/page-layout.module";
 
 const config: SocketIoConfig = {url: 'http://localhost:3100', options: {}}
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavBarComponent,
     FrontpageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SocketIoModule.forRoot(config),
-    FontAwesomeModule
+    FontAwesomeModule,
+    PageLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
