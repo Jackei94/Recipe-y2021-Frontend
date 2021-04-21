e88181a9047f386cd23ac2275d21827318aedfb9
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-04-21 13:17:06 +0200

    Created page-basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavBarComponent } from './app/page-basics/nav-bar/nav-bar/nav-bar.component';
+import { HeaderComponent } from './page-basics/header/header.component';
+import { FooterComponent } from './page-basics/footer/footer.component';
 
 const config: SocketIoConfig = {url: 'http://localhost:3100', options: {}}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
